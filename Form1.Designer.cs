3ddce6f2d4a2b6dec724efa7cfdfa46f92b30e6e
Kathlene <kathlenepanugam@gmail.com>
2022-06-04 16:27:39 +0800

    Tic Tac Toe

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Tic_Tac_Toe_Game_in_Visual_Basic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bac3 = new System.Windows.Forms.Button();
+            this.bac2 = new System.Windows.Forms.Button();
+            this.bac1 = new System.Windows.Forms.Button();
+            this.cab3 = new System.Windows.Forms.Button();
+            this.cab2 = new System.Windows.Forms.Button();
+            this.cab1 = new System.Windows.Forms.Button();
+            this.abc3 = new System.Windows.Forms.Button();
+            this.abc2 = new System.Windows.Forms.Button();
+            this.abc1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.bac3);
+            this.panel2.Controls.Add(this.bac2);
+            this.panel2.Controls.Add(this.bac1);
+            this.panel2.Controls.Add(this.cab3);
+            this.panel2.Controls.Add(this.cab2);
+            this.panel2.Controls.Add(this.cab1);
+            this.panel2.Controls.Add(this.abc3);
+            this.panel2.Controls.Add(this.abc2);
+            this.panel2.Controls.Add(this.abc1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(115, 211);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(295, 259);
+            this.panel2.TabIndex = 1;
+            // 
+            // bac3
+            // 
+            this.bac3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bac3.Location = new System.Drawing.Point(200, 176);
+            this.bac3.Name = "bac3";
+            this.bac3.Size = new System.Drawing.Size(94, 82);
+            this.bac3.TabIndex = 10;
+            this.bac3.UseVisualStyleBackColor = true;
+            this.bac3.Click += new System.EventHandler(this.bac3_Click);
+            // 
+            // bac2
+            // 
+            this.bac2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bac2.Location = new System.Drawing.Point(100, 176);
+            this.bac2.Name = "bac2";
+            this.bac2.Size = new System.Drawing.Size(94, 82);
+            this.bac2.TabIndex = 9;
+            this.bac2.UseVisualStyleBackColor = true;
+            this.bac2.Click += new System.EventHandler(this.bac2_Click);
+            // 
+            // bac1
+            // 
+            this.bac1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bac1.Location = new System.Drawing.Point(0, 176);
+            this.bac1.Name = "bac1";
+            this.bac1.Size = new System.Drawing.Size(94, 82);
+            this.bac1.TabIndex = 8;
+            this.bac1.UseVisualStyleBackColor = true;
+            this.bac1.Click += new System.EventHandler(this.bac1_Click);
+            // 
+            // cab3
+            // 
+            this.cab3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cab3.Location = new System.Drawing.Point(200, 88);
+            this.cab3.Name = "cab3";
+            this.cab3.Size = new System.Drawing.Size(94, 82);
+            this.cab3.TabIndex = 7;
+            this.cab3.UseVisualStyleBackColor = true;
+            this.cab3.Click += new System.EventHandler(this.cab3_Click);
+            // 
+            // cab2
+            // 
+            this.cab2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cab2.Location = new System.Drawing.Point(100, 88);
+            this.cab2.Name = "cab2";
+            this.cab2.Size = new System.Drawing.Size(94, 82);
+            this.cab2.TabIndex = 6;
+            this.cab2.UseVisualStyleBackColor = true;
+            this.cab2.Click += new System.EventHandler(this.cab2_Click);
+            // 
+            // cab1
+            // 
+            this.cab1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cab1.Location = new System.Drawing.Point(0, 88);
+            this.cab1.Name = "cab1";
+            this.cab1.Size = new System.Drawing.Size(94, 82);
+            this.cab1.TabIndex = 5;
+            this.cab1.UseVisualStyleBackColor = true;
+            this.cab1.Click += new System.EventHandler(this.cab1_Click);
+            // 
+            // abc3
+            // 
+            this.abc3.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.abc3.Location = new System.Drawing.Point(200, 0);
+            this.abc3.Name = "abc3";
+            this.abc3.Size = new System.Drawing.Size(94, 82);
+            this.abc3.TabIndex = 4;
+            this.abc3.UseVisualStyleBackColor = true;
+            this.abc3.Click += new System.EventHandler(this.abc3_Click);
+            // 
+            // abc2
+            // 
+            this.abc2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.abc2.Location = new System.Drawing.Point(100, 0);
+            this.abc2.Name = "abc2";
+            this.abc2.Size = new System.Drawing.Size(94, 82);
+            this.abc2.TabIndex = 3;
+            this.abc2.UseVisualStyleBackColor = true;
+            this.abc2.Click += new System.EventHandler(this.abc2_Click);
+            // 
+            // abc1
+            // 
+            this.abc1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.abc1.Location = new System.Drawing.Point(0, 0);
+            this.abc1.Name = "abc1";
+            this.abc1.Size = new System.Drawing.Size(94, 82);
+            this.abc1.TabIndex = 2;
+            this.abc1.UseVisualStyleBackColor = true;
+            this.abc1.Click += new System.EventHandler(this.abc1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(0, 0);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(537, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuGameToolStripMenuItem
+            // 
+            this.menuGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuGameToolStripMenuItem.Name = "menuGameToolStripMenuItem";
+            this.menuGameToolStripMenuItem.Size = new System.Drawing.Size(103, 24);
+            this.menuGameToolStripMenuItem.Text = "Menu Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tic_Tac_Toe_Game_in_Visual_Basic.Properties.Resources._4b261f5fe2a4dbb63e7db5a40622dc99;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(539, 464);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(137, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 54);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundImage = global::Tic_Tac_Toe_Game_in_Visual_Basic.Properties.Resources._4b261f5fe2a4dbb63e7db5a40622dc99;
+            this.ClientSize = new System.Drawing.Size(537, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe Game";
+            this.panel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Panel panel2;
+        private Button button2;
+        private Button bac3;
+        private Button bac2;
+        private Button bac1;
+        private Button cab3;
+        private Button cab2;
+        private Button cab1;
+        private Button abc3;
+        private Button abc2;
+        private Button abc1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuGameToolStripMenuItem;
+        private ToolStripMenuItem newGameToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,7 @@
+namespace Tic_Tac_Toe_Game_in_Visual_Basic
+{
+    public partial class Form1 : Form
+    {
+        
+    }
+}
